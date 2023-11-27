306cdc1f0c16a7aa390f2eb30ebb70443adbca0e
Ayman <ayman2002rahman@gmail.com>
2023-11-27 02:21:30 -0600

    Updated training code

--- snake_ai.py
+++ snake_ai.py
@@ -15,16 +15,16 @@ def epsilon_greedy_policy(q_table, state, epsilon):
         action = random.randint(0, env.action_space-1)
     return action
 
-train_episodes = 10000
+train_episodes = 500
 learning_rate = 0.7
 
-eval_episodes = 10
+eval_episodes = 100
 max_steps = 1000
 gamma = 0.95
 
 max_epsilon = 1
 min_epsilon = 0.05
-decay_rate = 0.0005
+decay_rate = 0.01
 
 for episode in tqdm(range(train_episodes)):
     epsilon = min_epsilon + (max_epsilon - min_epsilon) * np.exp(-decay_rate * episode)
@@ -41,4 +41,32 @@ for episode in tqdm(range(train_episodes)):
         if terminated:
             break
 
+        state = new_state
+
 print(q_table)
+print(np.max(q_table))
+
+episode_rewards = []
+
+for episode in tqdm(range(eval_episodes)):
+    state = env.reset()
+
+    step = 0
+    terminated = False
+    total_rewards_ep = 0
+
+    for step in range(max_steps):
+        action = greedy_policy(q_table, state)
+        new_state, reward, terminated = env.step(action)
+        total_rewards_ep += reward
+
+        if terminated or truncated:
+            break
+        state = new_state
+
+    episode_rewards.append(total_rewards_ep)
+
+mean_reward = np.mean(episode_rewards)
+std_reward = np.std(episode_rewards)
+
+print(f'Mean Reward: {mean_reward:.2f} +/- {std_reward:.2f}')
