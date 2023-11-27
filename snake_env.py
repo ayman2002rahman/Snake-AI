36b2a6332c5c8ff9a7cf6c9db61e3f56ae55e433
Ayman Rahman <ayman2002rahman@gmail.com>
2023-11-26 18:18:44 -0600

    Finished creating snake enviornment

--- snake_env.py
+++ snake_env.py
@@ -20,7 +20,13 @@ class Action(Enum):
 BOARD_WIDTH = 50
 BOARD_HEIGHT = 50
 
-class Snake_Env(self):
+class Snake_Env():
+
+	def generate_food(self):
+		self.food = (random.randint(0, BOARD_WIDTH-1), random.randint(0, BOARD_HEIGHT-1))
+		while self.food not in self.positions:
+			self.food = (random.randint(0, BOARD_WIDTH-1), random.randint(0, BOARD_HEIGHT-1))
+	
 	def __init__(self):
 		self.positions = [(BOARD_WIDTH // 2, BOARD_HEIGHT // 2)]
 		self.food = (random.randint(0, BOARD_WIDTH-1), random.randint(0, BOARD_HEIGHT-1))
@@ -29,8 +35,16 @@ class Snake_Env(self):
 		self.direction = Direction.RIGHT
 		self.score = 0
 
+	def reset(self):
+		self.positions = [(BOARD_WIDTH // 2, BOARD_HEIGHT // 2)]
+		self.food = (random.randint(0, BOARD_WIDTH-1), random.randint(0, BOARD_HEIGHT-1))
+		while self.food not in self.positions:
+			self.food = (random.randint(0, BOARD_WIDTH-1), random.randint(0, BOARD_HEIGHT-1))
+		self.direction = Direction.RIGHT
+		self.score = 0
+
 	def head(self):
-		return self.positions[0]
+		return self.positions[-1]
 	
 	def get_board(self): #get board 2d array
 		board = [[Board.NOTHING for j in range(BOARD_HEIGHT)] for i in range(BOARD_WIDTH)]
@@ -39,48 +53,62 @@ class Snake_Env(self):
 		board[self.food[0]][self.food[1]] = Board.FOOD
 		return board
 
+	def collision(self, position):
+		return position[0] < 0 or position[0] >= BOARD_WIDTH or position[1] < 0 or position[1] >= BOARD_HEIGHT or position in self.positions
+
 	def get_state(self):
+		left_collision = self.collision(self.head()+(-1,0))
+		right_collision = self.collision(self.head()+(1,0))
+		up_collision = self.collision(self.head()+(0,-1))
+		down_collsion = self.collision(self.head()+(0,1))
+
 		if self.direction is Direction.LEFT:
-			danger_straight = self.head()[0] - 1 < 0 or self.head()+(-1, 0) in self.positions
-			danger_left = self.head()[1] + 1 >= BOARD_HEIGHT or self.head()+(0, 1) in self.positions
-			danger_right = self.head()[1] - 1 < 0 or self.head()+(0, -1) in self.positions
+			danger_straight = left_collision
+			danger_left = down_collsion
+			danger_right = up_collision
 		elif self.direction is Direction.RIGHT:
-			danger_straight = self.head()[0] + 1 >= BOARD_WIDTH or self.head()+(1, 0) in self.positions
-			danger_left = self.head()[1] - 1 < 0 or self.head()+(0, -1) in self.positions
-			danger_right = self.head()[1] + 1 >= BOARD_HEIGHT or self.head()+(0, 1) in self.positions
+			danger_straight = right_collision
+			danger_left = up_collision
+			danger_right = down_collsion
 		elif self.direction is Direction.UP:
-			danger_straight = self.head()[1] - 1 < 0 or self.head()+(0, -1) in self.positions
-			danger_left = self.head()[0] - 1 < 0 or self.head()+(-1, 0) in self.positions
-			danger_right = self.head()[0] + 1 >= BOARD_WIDTH or self.head()+(1, 0) in self.positions
+			danger_straight = up_collision
+			danger_left = left_collision
+			danger_right = right_collision
 		elif self.direction is Direction.DOWN:
-			danger_straight = self.head()[0] - 1 < 0 or self.head()+(0, 1) in self.positions
-			danger_left = self.head()[1] + 1 >= BOARD_HEIGHT or self.head()+(1, 0) in self.positions
-			danger_right = self.head()[1] - 1 < 0 or self.head()+(-1, 0) in self.positions
+			danger_straight = down_collsion
+			danger_left = right_collision
+			danger_right = left_collision
 
 		food_left = self.food[0] < self.head()[0]
 		food_right = self.food[0] > self.head()[0]
 		food_up = self.food[1] < self.head()[1]
-		food_down = self.food[1] > self.head[1]
+		food_down = self.food[1] > self.head()[1]
 
 		return (self.direction, danger_straight, danger_left, danger_right, food_left, food_right, food_up, food_down)
 
 	def step(self, action):
 		reward = 0
+		done = False
+
 		if self.direction is Direction.LEFT and action is Action.STRAIGHT or self.direction is Direction.DOWN and action is Action.RIGHT or self.direction is Direction.UP and action is Action.LEFT:
-			if self.head[0] - 1 < 0 or self.board[self.head[0] - 1, self.head[1]] is Board.SNAKE:
-				reward = -10
-				done = True
-			else:
-				#update the head and tail
-				self.direction = Direction.LEFT
-				head += (-1, 0)
-				tail += (-1, 0)
+			new_position = (self.head()+(-1,0))
+		elif self.direction is Direction.RIGHT and action is Action.STRAIGHT or self.direction is Direction.DOWN and action is Action.LEFT or self.direction is Direction.UP and action is Action.RIGHT:
+			new_position = (self.head()+(1,0))
+		elif self.direction is Direction.UP and action is Action.STRAIGHT or self.direction is Direction.LEFT and action is Action.RIGHT or self.direction is Direction.RIGHT and action is Action.LEFT:
+			new_position = (self.head()+(0,-1))
+		elif self.direction is Direction.DOWN and action is Action.STRAIGHT or self.direction is Direction.LEFT and action is Action.LEFT or self.direction is Direction.RIGHT and action is Action.RIGHT:
+			new_position = (self.head()+(0,1))
 
-
-		elif action is Action.LEFT:
-			pass
-
-		elif action is Action.RIGHT:
-			pass
+		if self.collision(new_position):
+			reward = -10
+			done = True
+		elif new_position == self.food: # food condition here
+			reward = 10
+			score += 1
+			self.position.append(new_position)
+			self.generate_food()
+		else:
+			self.position.append(new_position)
+			self.pop(0)
 
 		return (self.get_state(), reward, done)
